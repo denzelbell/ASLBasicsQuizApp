d63c2df9710d33dd51df913af8cfcc62576ce5eb
denzelbell <dnzlbell@gmail.com>
2021-03-20 01:05:56 -0500

    - Removed notification bar

--- app/src/main/java/com/example/aslbasics/MainActivity.kt
+++ app/src/main/java/com/example/aslbasics/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.aslbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
