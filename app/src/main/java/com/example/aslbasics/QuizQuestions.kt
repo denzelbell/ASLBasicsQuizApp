1af50296be438e613fb1520c7ee3f25f9abf4727
denzelbell <dnzlbell@gmail.com>
2021-03-20 14:52:19 -0500

    - Created new page to transition to - Setup page layout

---
+++ app/src/main/java/com/example/aslbasics/QuizQuestions.kt
@@ -0,0 +1,11 @@
+package com.example.aslbasics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizQuestions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+    }
+}
