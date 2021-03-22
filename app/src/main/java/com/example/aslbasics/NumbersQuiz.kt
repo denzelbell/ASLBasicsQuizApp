a4eb26adcaa73a24f07908be4479a118ae0f35b8
denzelbell <dnzlbell@gmail.com>
2021-03-22 02:15:19 -0500

    - Added numbers page

--- app/src/main/java/com/example/aslbasics/NumbersQuiz.kt
+++ app/src/main/java/com/example/aslbasics/NumbersQuiz.kt
@@ -2,10 +2,15 @@ package com.example.aslbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class NumbersQuiz : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_numbers_quiz)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+
     }
 }
