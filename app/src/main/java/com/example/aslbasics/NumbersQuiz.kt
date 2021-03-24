3b8f6bee60076861ade4ebd40b6e4ba60cae0508
denzelbell <dnzlbell@gmail.com>
2021-03-24 01:24:21 -0500

    - Updated final changes

--- app/src/main/java/com/example/aslbasics/NumbersQuiz.kt
+++ app/src/main/java/com/example/aslbasics/NumbersQuiz.kt
@@ -8,172 +8,174 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.core.content.ContextCompat
-import kotlinx.android.synthetic.main.activity_numbers_quiz.*
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
+
 
 class NumbersQuiz : AppCompatActivity(), View.OnClickListener {
 
-    private var nCurrentPosition: Int = 1
-    private var nQuestionList: ArrayList<Questions>? = null
-    private var nSelectedOptionPosition: Int = 0
-    private var nCorrectAnswers: Int = 0
-    private var nUserName: String? = null
+    private var aCurrentPosition: Int = 1
+    private var aQuestionList: ArrayList<Questions>? = null
+    private var aSelectedOptionPosition: Int = 0
+    private var aCorrectAnswers: Int = 0
+    private var aUserName: String? = null
 
+    // App functionality
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_numbers_quiz)
+        setContentView(R.layout.activity_quiz_questions)
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
-        nUserName = intent.getStringExtra(Constants.USER_NAME)
+        aUserName = intent.getStringExtra(Constants.USER_NAME)
 
-        nQuestionList = Constants.getNumQuestions()
+        aQuestionList = Constants.getNumQuestions()
 
-        setNumQuestion()
+        setQuestion()
 
-        tv_num_opt1.setOnClickListener(this)
-        tv_num_opt2.setOnClickListener(this)
-        tv_num_opt3.setOnClickListener(this)
+        tv_opt1.setOnClickListener(this)
+        tv_opt2.setOnClickListener(this)
+        tv_opt3.setOnClickListener(this)
 
-        btn_num_submit.setOnClickListener(this)
+        btn_submit.setOnClickListener(this)
     }
 
     // Give values to the answer options
-    private fun defaultNumOptView() {
+    private fun defaultOptView() {
         val opts = ArrayList<TextView>()
-        opts.add(0, tv_num_opt1)
-        opts.add(1, tv_num_opt2)
-        opts.add(2, tv_num_opt3)
+        opts.add(0, tv_opt1)
+        opts.add(1, tv_opt2)
+        opts.add(2, tv_opt3)
 
         for (opt in opts){
             opt.setTextColor((Color.parseColor("#7A8089")))
             opt.typeface = Typeface.DEFAULT
             opt.background = ContextCompat.getDrawable(
-                    this,
-                    R.drawable.default_option_border_bg
+                this,
+                R.drawable.default_option_border_bg
             )
         }
     }
 
     // Sets up the questions
-    private fun setNumQuestion() {
-        val question = nQuestionList!![nCurrentPosition - 1]
+    private fun setQuestion() {
+        val question = aQuestionList!![aCurrentPosition - 1]
 
-        defaultNumOptView()
+        defaultOptView()
 
-        if (nCurrentPosition == 10) {
-            btn_num_submit.text = "FINISH"
+        if (aCurrentPosition == 10) {
+            btn_submit.text = "FINISH"
         } else {
-            btn_num_submit.text = "SUBMIT"
+            btn_submit.text = "SUBMIT"
         }
         // Sets up progress bar and side text
-        progressNumBar.progress = nCurrentPosition
-        tv_progress_num.text = "${nCurrentPosition}/${progressNumBar.max}"
+        progressBar.progress = aCurrentPosition
+        tv_progress.text = "${aCurrentPosition}/${progressBar.max}"
 
         // set the question equal to id
-        tv_num_question.text = question!!.questions
+        tv_question.text = question!!.questions
 
         // set image
-        iv_num_sign.setImageResource(question.image)
+        iv_sign.setImageResource(question.image)
 
         // set text
-        tv_num_opt1.text = question.opt1
-        tv_num_opt2.text = question.opt2
-        tv_num_opt3.text = question.opt3
+        tv_opt1.text = question.opt1
+        tv_opt2.text = question.opt2
+        tv_opt3.text = question.opt3
     }
 
     // Sets up click actions
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.tv_num_opt1 -> {
-                selectedNumOptionView(tv_num_opt1, 1)
+            R.id.tv_opt1 -> {
+                selectedOptionView(tv_opt1, 1)
             }
-            R.id.tv_num_opt2 -> {
-                selectedNumOptionView(tv_num_opt2, 2)
+            R.id.tv_opt2 -> {
+                selectedOptionView(tv_opt2, 2)
             }
-            R.id.tv_num_opt3 -> {
-                selectedNumOptionView(tv_num_opt3, 3)
+            R.id.tv_opt3 -> {
+                selectedOptionView(tv_opt3, 3)
             }
-            R.id.btn_num_submit ->{
-                if(nSelectedOptionPosition == 0){
-                    nCurrentPosition++
+            R.id.btn_submit ->{
+                if(aSelectedOptionPosition == 0){
+                    aCurrentPosition++
 
                     when{
-                        nCurrentPosition <= 10 ->{
+                        aCurrentPosition <= 10 ->{
 
-                            setNumQuestion()
-                            tv_num_opt1.isClickable = true
-                            tv_num_opt2.isClickable = true
-                            tv_num_opt3.isClickable = true
+                            setQuestion()
+                            tv_opt1.isClickable = true
+                            tv_opt2.isClickable = true
+                            tv_opt3.isClickable = true
 
                         } else ->{
                         val intent = Intent(this, Results::class.java)
-                        intent.putExtra(Constants.USER_NAME, nUserName)
-                        intent.putExtra(Constants.CORRECT_ANSWERS, nCorrectAnswers)
+                        intent.putExtra(Constants.USER_NAME, aUserName)
+                        intent.putExtra(Constants.CORRECT_ANSWERS, aCorrectAnswers)
                         intent.putExtra(Constants.TOTAL_QUESTIONS, 10)
                         startActivity(intent)
                         finish()
                     }
                     }
                 } else {
-                    val question = nQuestionList?.get(nCurrentPosition - 1)
+                    val question = aQuestionList?.get(aCurrentPosition - 1)
 
 
-                    if (question!!.correctAnswer != nSelectedOptionPosition) {
-                        answerNumView(nSelectedOptionPosition, R.drawable.wrong_option_border_bg)
-                        tv_num_opt1.isClickable = false
-                        tv_num_opt2.isClickable = false
-                        tv_num_opt3.isClickable = false
+                    if (question!!.correctAnswer != aSelectedOptionPosition) {
+                        answerView(aSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                        tv_opt1.isClickable = false
+                        tv_opt2.isClickable = false
+                        tv_opt3.isClickable = false
                     } else {
-                        nCorrectAnswers++
+                        aCorrectAnswers++
                     }
-                    answerNumView(question.correctAnswer, R.drawable.correct_option_border_bg)
-                    tv_num_opt1.isClickable = false
-                    tv_num_opt2.isClickable = false
-                    tv_num_opt3.isClickable = false
+                    answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+                    tv_opt1.isClickable = false
+                    tv_opt2.isClickable = false
+                    tv_opt3.isClickable = false
 
-                    if (nCurrentPosition == 10) {
-                        btn_num_submit.text = "FINISH"
+                    if (aCurrentPosition == 10) {
+                        btn_submit.text = "FINISH"
                     } else {
-                        btn_num_submit.text = "GO TO NEXT QUESTION"
+                        btn_submit.text = "GO TO NEXT QUESTION"
                     }
-                    nSelectedOptionPosition = 0
+                    aSelectedOptionPosition = 0
                 }
             }
         }
     }
 
     // Responsible for answer colors
-    private fun answerNumView(answer: Int, drawablesView: Int){
+    private fun answerView(answer: Int, drawablesView: Int){
         when(answer){
             1 -> {
-                tv_num_opt1.background = ContextCompat.getDrawable(
-                        this, drawablesView
+                tv_opt1.background = ContextCompat.getDrawable(
+                    this, drawablesView
                 )
             }
             2 -> {
-                tv_num_opt2.background = ContextCompat.getDrawable(
-                        this, drawablesView
+                tv_opt2.background = ContextCompat.getDrawable(
+                    this, drawablesView
                 )
             }
             3 -> {
-                tv_num_opt3.background = ContextCompat.getDrawable(
-                        this, drawablesView
+                tv_opt3.background = ContextCompat.getDrawable(
+                    this, drawablesView
                 )
             }
         }
     }
 
     // Function that tells the app what the options should look like if selected
-    private fun selectedNumOptionView(tv: TextView, selectedOptionNum: Int){
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int){
 
-        defaultNumOptView()
-        nSelectedOptionPosition = selectedOptionNum
+        defaultOptView()
+        aSelectedOptionPosition = selectedOptionNum
 
         tv.setTextColor((Color.parseColor("#363A43")))
         tv.setTypeface(tv.typeface, Typeface.BOLD)
         tv.background = ContextCompat.getDrawable(
-                this,
-                R.drawable.select_option_border_bg
+            this,
+            R.drawable.select_option_border_bg
         )
     }
 }
